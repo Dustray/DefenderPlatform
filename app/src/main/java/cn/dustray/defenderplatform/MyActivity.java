e3519bf38958fd50ee15e7e53ba47b15cefa3c67
Dustray <yinxiyue1995@outlook.com>
2019-05-27 14:34:21 +0800

    登录注册我的   样式

--- app/src/main/java/cn/dustray/defenderplatform/MyActivity.java
+++ app/src/main/java/cn/dustray/defenderplatform/MyActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.hyphenate.EMCallBack;
 import com.hyphenate.EMError;
@@ -12,18 +13,37 @@ import com.hyphenate.chat.EMClient;
 import com.hyphenate.exceptions.HyphenateException;
 
 import cn.bmob.v3.BmobUser;
+import cn.dustray.entity.UserEntity;
 import cn.dustray.utils.xToast;
 
 public class MyActivity extends AppCompatActivity implements View.OnClickListener {
-private Button btnExit;
+    private Button btnExit;
+    private TextView username, email, emailVerify, identity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        btnExit=findViewById(R.id.btn_exit);
+        btnExit = findViewById(R.id.btn_exit);
         btnExit.setOnClickListener(this);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        emailVerify = findViewById(R.id.email_Verify);
+        identity = findViewById(R.id.identity);
+
+        initData();
     }
+
+    private void initData() {
+        if (!BmobUser.isLogin()) return;
+        UserEntity u = BmobUser.getCurrentUser(UserEntity.class);
+        username.setText(u.getUsername());
+        email.setText(u.getEmail());
+        emailVerify.setText(u.getEmailVerified() ? "已验证" : "未验证");
+        identity.setText(u.isGuardian() ? "监护人" : "被监护人");
+    }
+
     public void logoutEaseMob() {
         new Thread(new Runnable() {
             public void run() {
@@ -50,9 +70,10 @@ private Button btnExit;
             }
         }).start();
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId ();
+        int id = item.getItemId();
         if (id == android.R.id.home) {
             finish();
             return true;
@@ -62,8 +83,7 @@ private Button btnExit;
 
     @Override
     public void onClick(View view) {
-        switch ((view.getId()))
-        {
+        switch ((view.getId())) {
             case R.id.btn_exit:
                 BmobUser.logOut();
                 logoutEaseMob();
